6ae59c3a2caf0effae874ee4aa11cedcfadd75d4
wasw100 <wasw100@gmail.com>
2015-04-22 12:07:58 +0800

    format code for pep8

--- sublime-plugin/FileUpload.py
+++ sublime-plugin/FileUpload.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
-import sublime, sublime_plugin
-import os.path, sys, threading, time, functools, zlib
+import sublime
+import sublime_plugin
+import os.path
+import sys
+import threading
+import time
+import functools
+import zlib
 import mimetypes
 import codecs
 try:
@@ -12,13 +18,11 @@ _ver = sys.version_info
 
 is_py2 = (_ver[0] == 2)
 is_py3 = (_ver[0] == 3)
-is_before_py26 = (is_py2 and _ver[1]<=6)
+is_before_py26 = (is_py2 and _ver[1] <= 6)
 
 if is_py2:
-    from StringIO import StringIO
     text_type = unicode
 elif is_py3:
-    from io import StringIO
     text_type = str
 
 if is_before_py26:
@@ -28,17 +32,19 @@ else:
 
 writer = codecs.lookup('utf-8')[3]
 
+
 class FileUploadCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         sublime.status_message(u'开始上传 %s' % self.view.file_name())
         print(self.view.file_name())
         file_name = str(self.view.file_name())
         config_key = None
-        
+
         self.settings = sublime.load_settings('FileUpload.sublime-settings')
         local_prefix = self.settings.get('local_prefix')
         if not local_prefix:
-            sublime.status_message('Setting中没有找到local_prefix, 在FileUpload->Settings设置')
+            msg = 'Setting中没有找到local_prefix, 在FileUpload->Settings设置'
+            sublime.status_message(msg)
         else:
             for key in local_prefix:
                 real_key = os.path.realpath(key)
@@ -51,14 +57,19 @@ class FileUploadCommand(sublime_plugin.TextCommand):
                 upload_url = upload_config['upload_url']
                 local_prefix = os.path.realpath(config_key)
                 remote_prefix = upload_config['remote_prefix']
-                t = UploadThread(upload_url, file_name, local_prefix, remote_prefix)
+                t = UploadThread(upload_url, file_name,
+                                 local_prefix, remote_prefix)
                 t.start()
             else:
-                sublime.status_message(u'没有匹配的配置文件, 可以ctrl+`在Console复制路径 然后在FileUpload->Settings-User设置')
+                msg = (u'没有匹配的配置文件, 可以ctrl+`在Console复制路径'
+                       u'然后在FileUpload->Settings-User设置')
+                sublime.status_message(msg)
 
 
 def show_finish(url, remote_path, use_time):
-    sublime.status_message(u'上传成功, 服务器-%s, 路径-%s, 使用-%dms' % (url, remote_path, use_time))
+    msg = u'上传成功, 服务器-{0}, 路径-{1}, 使用-{2}ms' \
+        .format(url, remote_path, use_time)
+    sublime.status_message(msg)
 
 
 class UploadThread(threading.Thread):
@@ -67,16 +78,16 @@ class UploadThread(threading.Thread):
         self.upload_url = upload_url
         self.file_name = file_name
 
-        local_prefix_length = len(local_prefix)
+        local_prefix_len = len(local_prefix)
         if local_prefix.endswith('/'):
-            local_prefix_lenth -= 1
-        remote_path = os.path.join(remote_prefix, file_name[local_prefix_len+1:])
+            local_prefix_len -= 1
+        remote_path = os.path.join(remote_prefix,
+                                   file_name[local_prefix_len+1:])
 
         self.remote_path = remote_path.replace('\\', '/')
         super(UploadThread, self).__init__()
-    
+
     def run(self):
-        data = dict(path=self.remote_path)
         zlib_data = zlib.compress(open(self.file_name, 'rb').read(), 5)
         files = dict(file=zlib_data)
         begin = time.time()
@@ -92,21 +103,24 @@ class UploadThread(threading.Thread):
             request = urllib2.Request(self.upload_url, body, headers)
             print(urllib2.urlopen(request).read())
 
-            # r = requests.post(self.upload_url, files=files, data=data, timeout=30)
             use_time = (time.time() - begin) * 1000
-            callback = functools.partial(show_finish, self.upload_url, self.remote_path, use_time)
+            callback = functools.partial(show_finish, self.upload_url,
+                                         self.remote_path, use_time)
             sublime.set_timeout(callback, 0)
         except Exception as e:
-            sublime.error_message(u'上传失败, 确认服务器-%s 可以访问,\n 错误信息:%s' % (self.upload_url, e))
+            msg = u'上传失败, 确认服务器-{0} 可以访问,\n 错误信息:{1}' \
+                .format(self.upload_url, e)
+            sublime.error_message(msg)
             raise e
 
 
-#for gen upload data
-#from http://code.activestate.com/recipes/146306/
+# for gen upload data
+# from http://code.activestate.com/recipes/146306/
 def encode_multipart_formdata(fields, files):
     """
     fields is a sequence of (name, value) elements for regular form fields.
-    files is a sequence of (name, filename, value) elements for data to be uploaded as files
+    files is a sequence of (name, filename, value) elements for data to be
+    uploaded as files
     Return (content_type, body) ready for httplib.HTTP instance
     """
     # BOUNDARY = mimetools.choose_boundary()
@@ -120,7 +134,8 @@ def encode_multipart_formdata(fields, files):
         L.append(value)
     for (key, filename, value) in files:
         L.append('--' + BOUNDARY)
-        L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
+        template = 'Content-Disposition: form-data; name="{0}"; filename="{1}"'
+        L.append(template.format(key, filename))
         L.append('Content-Type: %s' % get_content_type(filename))
         L.append('')
         L.append(value)
@@ -136,6 +151,7 @@ def encode_multipart_formdata(fields, files):
 
     content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
     return content_type, body.getvalue()
+
 
 def get_content_type(filename):
     return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
